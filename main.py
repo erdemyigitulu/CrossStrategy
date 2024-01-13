d7f90de818e9f3996436a18b6991814a02580e08
Erdolinyohuertas <erdemyigitulu@gmail.com>
2024-01-13 22:13:12 +0300

    iscrossemvalues function is written.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from config import *
 import requests
 from urllib.parse import urljoin
+import time
 
 baseUrl = "https://fapi.binance.com/"
 
@@ -10,38 +11,63 @@ def getCandles (symbol,interval,limit):
         "interval" : interval,
         "limit" : limit
     }
-
     url = urljoin(baseUrl,"fapi/v1/klines")
     response = requests.get(url, params).json()
     return response
 
-def calculateCloseTimes (symbol,day,intervalmin):
-    candleCount = int((60 / intervalmin) * 24 * day)
+# def getLivePrice(symbol)
+#     url = urljoin(baseUrl,"fapi/v1/klines")
+#     response = requests.get(url, params).json()
+#     return response
+
+def getCloseTimes (symbol,intervalmin):
+    candleCount = int((60 / intervalmin) * 24 * 3)
     interval = str(intervalmin) + "m"
-    testo = getCandles(symbol,interval,candleCount)
+    candles = getCandles(symbol,interval,candleCount)
     closeTimes = []
-    for candles in testo :
+    for candles in candles :
         closeTime = float(candles[4])
         closeTimes.append(closeTime)
     return closeTimes
 
-def calculate_ema(data,period):
-    ema_values = []
-    multiplier = 2 / (period + 1)
-    
-    # İlk EMA değeri, veri setinin ortalaması ile başlar
-    initial_ema = sum(data[:period]) / period
-    ema_values.append(initial_ema)
-    
-    # Diğer EMA değerleri hesaplanır
-    for i in range(period, len(data)):
-        ema = (data[i] - ema_values[-1]) * multiplier + ema_values[-1]
-        ema_values.append(ema)
-    print(ema_values)
-    return ema_values
+def calculateEma(data,day):
+    emaValues = []
+    multiplier = 2 / (day + 1)
+    initialEma = sum(data[:day]) / day
+    emaValues.append(initialEma)
+    for i in range(day, len(data)):
+        ema = (data[i] - emaValues[-1]) * multiplier + emaValues[-1]
+        emaValues.append(ema)
+    return emaValues
 
-data = calculateCloseTimes("btcusdt",5,15)    
-period = 5  
-calculate_ema(data,period)
 
+def calculatEmaValues () :
+    dayOfEma = [5,8,13]
+    emaDatasOfDays = []
+    for day in dayOfEma :
+        dataofDay = getCloseTimes("btcusdt",15)
+        emaDataOfDay = calculateEma(dataofDay,day)
+        emaDatasOfDays.append(emaDataOfDay)
+    return emaDatasOfDays
+    
+
+def isCrossEmaValues ():
+    emaValues = calculatEmaValues ()
+    ema5Last = emaValues[0][-2]
+    ema8Last = emaValues[1][-2]
+    ema13Last = emaValues[2][-2]
+    ema5SecondLast = emaValues[0][-3]
+    ema8SecondLast = emaValues[1][-3]
+    ema13SecondLast = emaValues[2][-3]
+    if ema5SecondLast > ema8SecondLast or ema5SecondLast > ema13SecondLast: 
+        if ema5Last < ema8Last and ema13Last:
+            print("SHORTLA ")
+    elif ema5SecondLast < ema8SecondLast or ema5SecondLast < ema13SecondLast :
+        if ema5Last > ema8Last and ema13Last :
+            print("LONGLA")
+
+isCrossEmaValues()
+
+
+    
 
